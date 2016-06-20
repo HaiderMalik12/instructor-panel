53d751960ac20f46be38be28119f2d4a924542ce
Haider Malik <haidermalik5044@gmail.com>
2016-06-21 04:20:47 +0500

    Inital Commit

---
+++ src/main.js
@@ -0,0 +1,41 @@
+$ = jQuery = require('jquery');
+var React = require('react');
+
+/*React Componenets*/
+var Home = require('./components/homePage');
+var Header = require('./components/common/header');
+var About = require('./components/about/aboutPage');
+
+(function (win) {
+    "use strict";
+    var App = React.createClass({
+
+        render: function render() {
+            var Child;
+            switch (this.props.route) {
+                case 'about':
+                    Child = About;
+                    break;
+                default:
+                    Child = Home;
+            }
+            return (
+                <div>
+                <Header/>
+                <Child/>
+                </div>
+            );
+        }
+
+    });
+
+    function renderWhenHashChanges() {
+        //skip the # tag from route http://localhost/#about
+        var route = win.location.hash.substr(1);
+        //Instantiate the App Component
+        React.render(<App route={route}/>, document.getElementById('app'));
+    }
+
+    win.addEventListener('hashchange', renderWhenHashChanges);
+    renderWhenHashChanges();
+})(window);
