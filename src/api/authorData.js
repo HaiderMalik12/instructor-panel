461bce0eba1a46c0de2a6d926a5e7152663d9989
Haider Malik <haidermalik5044@gmail.com>
2016-06-22 06:18:09 +0500

    Added Authors Mock Data

---
+++ src/api/authorData.js
@@ -0,0 +1,19 @@
+module.exports = {
+    authors: [
+        {
+            id: 'haider-malik',
+            firstName: 'Haider',
+            lastName: 'Malik'
+        },
+        {
+            id: 'janny-khan',
+            firstName: 'Janny',
+            lastName: 'Khan'
+        },
+        {
+            id: 'dani-ali',
+            firstName: 'Dani',
+            lastName: 'Ali'
+        }
+    ]
+}
